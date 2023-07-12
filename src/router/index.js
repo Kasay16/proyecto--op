3f68cc8eee7f6cf8bc234e57fccf4614dc020c25
Miguel Orjuela <miguel.orjuela45@gmail.com>
2023-07-12 09:34:33 -0500

    laboratory options

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,17 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/laboratory",
+    component: () => import('@/layouts/default/Default.vue'),
+    children:[
+      {
+        path: "options",
+        name: "optionsLaboratory",
+        component: () => import('@/views/OptionsLaboratory.vue')
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/OptionsLaboratory.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <h1>Contador: {{ count }}</h1>
+    </v-row>
+    <v-row justify="center">
+      <v-btn @click="increment">Incremetar</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script>
